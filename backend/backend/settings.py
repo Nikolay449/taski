f63fa9da0abb93e1363d9ec246727e45cdf641dc
Николай Драй <ipkissstan@yandex.ru>
2023-07-04 13:06:36 +0300

    Add set

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,16 @@
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', '').split()
 
 
 # Application definition
